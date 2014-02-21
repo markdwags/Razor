e784c3bccda4fb59831a9b751faf80755610f575
Mark Sturgill <msturgill@gmail.com>
2014-02-20 20:41:50 -0700

    add versioning and tune build order

--- RPVOpenStub/resource.h
+++ RPVOpenStub/resource.h
@@ -11,6 +11,7 @@
 #define IDR_MAINFRAME                   128
 #define IDI_ICON1                       129
 #define IDI_MAINFRAME                   129
+#define IDR_VERSION2                    130
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -18,7 +19,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
