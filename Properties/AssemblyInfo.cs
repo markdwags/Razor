20c7f8a36af67246b8c95792414bc3d41db002aa
Mark Sturgill <msturgill@gmail.com>
2014-02-24 15:45:44 -0700

    update ui elements, move AssemblyInfo.cs version bump prep for 1.0.13.11

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersionAttribute("1.0.13.10")]
+[assembly: AssemblyVersionAttribute("1.0.13.11")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- UI/SplashScreen.cs
+++ UI/SplashScreen.cs
@@ -40,7 +40,7 @@ namespace Assistant
         private delegate void SetMsgDelegate(SplashScreen screen, string arg);
         private static void SetMessage(SplashScreen screen, string msg)
         {
-            screen.message.Text = msg;
+            //screen.message.Text = msg;
         }
 
         public static string MessageStr 
@@ -78,9 +78,7 @@ namespace Assistant
 			catch
 			{
 			}
-        }
-		private System.Windows.Forms.Label message;
-		private System.Windows.Forms.PictureBox donate;
+		}
 		private System.ComponentModel.Container components = null;
 
 		public SplashScreen()
@@ -116,56 +114,27 @@ namespace Assistant
 		/// </summary>
 		private void InitializeComponent()
 		{
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
-            this.message = new System.Windows.Forms.Label();
-            this.donate = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.donate)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // message
-            // 
-            this.message.BackColor = System.Drawing.Color.Transparent;
-            this.message.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.message.Location = new System.Drawing.Point(-16, 308);
-            this.message.Name = "message";
-            this.message.Size = new System.Drawing.Size(440, 21);
-            this.message.TabIndex = 1;
-            this.message.Text = "Don\'t forget to Donate!";
-            this.message.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // donate
-            // 
-            this.donate.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.donate.Image = ((System.Drawing.Image)(resources.GetObject("donate.Image")));
-            this.donate.Location = new System.Drawing.Point(374, 0);
-            this.donate.Name = "donate";
-            this.donate.Size = new System.Drawing.Size(50, 25);
-            this.donate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.donate.TabIndex = 2;
-            this.donate.TabStop = false;
-            this.donate.Click += new System.EventHandler(this.donate_Click);
-            // 
-            // SplashScreen
-            // 
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.BackColor = System.Drawing.Color.White;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(435, 331);
-            this.ControlBox = false;
-            this.Controls.Add(this.donate);
-            this.Controls.Add(this.message);
-            this.DoubleBuffered = true;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "SplashScreen";
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Razor Splash Screen";
-            this.TopMost = true;
-            this.TransparencyKey = System.Drawing.Color.White;
-            this.Load += new System.EventHandler(this.SplashScreen_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.donate)).EndInit();
-            this.ResumeLayout(false);
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+			this.SuspendLayout();
+			// 
+			// SplashScreen
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.BackColor = System.Drawing.Color.White;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.ClientSize = new System.Drawing.Size(435, 331);
+			this.ControlBox = false;
+			this.DoubleBuffered = true;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "SplashScreen";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Razor Splash Screen";
+			this.TopMost = true;
+			this.TransparencyKey = System.Drawing.Color.White;
+			this.Load += new System.EventHandler(this.SplashScreen_Load);
+			this.ResumeLayout(false);
 
 		}
 		#endregion
