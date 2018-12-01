01d9813455b52d09b53ca74d73e30b4091abee37
Jaedan <jaedan@none.com>
2018-11-30 22:04:19 -0700

    Add a new C++ library for miscellaneous windows stuff (WinUtil)
    Begin by moving the CaptureScreen function from Crypt to this library.

--- Crypt/gfx.cpp
+++ Crypt/gfx.cpp
@@ -24,9 +24,6 @@ DWMSETWINDOWATTRIBUTE zDwmSetWindowAttribute = NULL;
 HMODULE hThemes = NULL;
 HMODULE hDwmApi = NULL;
 
-HICON hRazorIcon = NULL;
-HFONT hRazorFont = NULL;
-
 void InitThemes()
 {
 	hThemes = LoadLibrary("UXTHEME.DLL");
@@ -369,81 +366,4 @@ void RedrawTitleBar( HWND hWnd, bool active )
 
 	ReleaseDC( hWnd, hDC );
 	ReleaseMutex( CommMutex );
-}
-
-void GetRazorFont()
-{
-	if ( !hRazorFont )
-		hRazorFont = CreateFont( -10, 0, 0, 0, FW_SEMIBOLD, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DRAFT_QUALITY, FIXED_PITCH, "Comic Sans MS" );
-}
-
-DLLFUNCTION HBITMAP CaptureScreen( BOOL full, const char *msg )
-{
-	HDC hScreen = NULL;
-	int destWidth, destHeight;
-	RECT rect;
-
-	//SetForegroundWindow( hUOWindow );
-	UpdateWindow( hUOWindow );
-
-	if ( full )
-	{
-		hScreen = GetDC( NULL );
-		rect.left = 0;
-		rect.top = 0;
-		destWidth = GetDeviceCaps( hScreen, HORZRES );
-		destHeight = GetDeviceCaps( hScreen, VERTRES );
-	}
-	else
-	{
-		hScreen = GetWindowDC( hUOWindow );
-		GetWindowRect( hUOWindow, &rect );
-		destWidth = rect.right - rect.left;
-		destHeight = rect.bottom - rect.top;
-	}
-
-	HDC hCap = CreateCompatibleDC( hScreen );
-	HBITMAP hBMP = CreateCompatibleBitmap( hScreen, destWidth, destHeight );
-	SelectObject( hCap, hBMP );
-
-	BitBlt( hCap, 0, 0, destWidth, destHeight, hScreen, 0, 0, SRCCOPY );
-	//StretchBlt( hCap, 0, 0, destWidth, destHeight, hScreen, rect.left, rect.top, srcWidth, srcHeight, SRCCOPY );
-
-	if ( !hRazorIcon )
-		hRazorIcon = LoadIcon( hInstance, MAKEINTRESOURCE(IDI_RAZOR) );
-
-	if ( hRazorIcon )
-	{
-		rect.left = destWidth - 32;
-		rect.right = destWidth;
-		rect.top = 0;
-		rect.bottom = 32;
-
-		//FillRect( hCap, &rect, (HBRUSH)GetStockObject( BLACK_BRUSH ) );
-		DrawIcon( hCap, rect.left, rect.top, hRazorIcon );
-	}
-
-	if ( msg != NULL && msg[0] != 0 )
-	{
-		SIZE text;
-		int len = (int)strlen(msg);
-		SelectObject( hCap, hRazorFont );
-		SetTextColor( hCap, RGB(255,255,255) );
-		SetBkMode( hCap, TRANSPARENT );
-
-		GetTextExtentPoint32( hCap, msg, len, &text );
-		
-		rect.top = 0;
-		rect.bottom = text.cy + 4;
-		rect.right = destWidth - 32;
-		rect.left = destWidth - 32 - text.cx - 4;
-		FillRect( hCap, &rect, (HBRUSH)GetStockObject( BLACK_BRUSH ) );
-
-		TextOut( hCap, rect.left + 2, 2, msg, len );
-	}
-
-	ReleaseDC( full ? NULL : hUOWindow, hScreen );
-	DeleteDC( hCap );
-
-	return hBMP;
 }
---
+++ WinUtil/WinUtil.cpp
@@ -0,0 +1,83 @@
+#include "WinUtil.h"
+
+HINSTANCE hInstance = NULL;
+HICON hRazorIcon = NULL;
+HFONT hRazorFont = NULL;
+
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD dwReason, LPVOID)
+{
+	hInstance = (HMODULE)hModule;
+
+	switch (dwReason) {
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hInstance);
+		break;
+
+	case DLL_PROCESS_DETACH:
+		break;
+
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+		break;
+	}
+
+	return TRUE;
+}
+
+DLLFUNCTION HBITMAP CaptureScreen(HWND hUOWindow, BOOL full, const char *msg)
+{
+	HDC hScreen = NULL;
+	int destWidth, destHeight;
+	RECT rect;
+
+	if (!hRazorFont)
+		hRazorFont = CreateFont(-10, 0, 0, 0, FW_SEMIBOLD, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DRAFT_QUALITY, FIXED_PITCH, "Comic Sans MS");
+
+	UpdateWindow(hUOWindow);
+
+	if (full)
+	{
+		hScreen = GetDC(NULL);
+		rect.left = 0;
+		rect.top = 0;
+		destWidth = GetDeviceCaps(hScreen, HORZRES);
+		destHeight = GetDeviceCaps(hScreen, VERTRES);
+	}
+	else
+	{
+		hScreen = GetWindowDC(hUOWindow);
+		GetWindowRect(hUOWindow, &rect);
+		destWidth = rect.right - rect.left;
+		destHeight = rect.bottom - rect.top;
+	}
+
+	HDC hCap = CreateCompatibleDC(hScreen);
+	HBITMAP hBMP = CreateCompatibleBitmap(hScreen, destWidth, destHeight);
+	SelectObject(hCap, hBMP);
+
+	BitBlt(hCap, 0, 0, destWidth, destHeight, hScreen, 0, 0, SRCCOPY);
+
+	if (msg != NULL && msg[0] != 0)
+	{
+		SIZE text;
+		int len = (int)strlen(msg);
+		SelectObject(hCap, hRazorFont);
+		SetTextColor(hCap, RGB(255, 255, 255));
+		SetBkMode(hCap, TRANSPARENT);
+
+		GetTextExtentPoint32(hCap, msg, len, &text);
+
+		rect.top = 0;
+		rect.bottom = text.cy + 4;
+		rect.right = destWidth - 32;
+		rect.left = destWidth - 32 - text.cx - 4;
+		FillRect(hCap, &rect, (HBRUSH)GetStockObject(BLACK_BRUSH));
+
+		TextOut(hCap, rect.left + 2, 2, msg, len);
+	}
+
+	ReleaseDC(full ? NULL : hUOWindow, hScreen);
+	DeleteDC(hCap);
+
+	return hBMP;
+}
