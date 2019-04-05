8bdbabaf934e7826fe72f81a989759d4bc73b4ff
Jaedan <jaedan@none.com>
2019-04-05 00:04:37 -0700

    Merge UOArt.h into UOArt.cpp

--- Crypt/UOArt.cpp
+++ Crypt/UOArt.cpp
@@ -1,10 +1,45 @@
 #include "stdafx.h"
-#include "UOArt.h"
 #include "Crypt.h"
 #include "Resource.h"
 #include <uxtheme.h>
 #include <vssym32.h>
 #include <dwmapi.h>
+
+#define Color16to32(c16) (((c16) & 0x7C00) >> 7) | (((c16) & 0x3E0) << 6) | (((c16) & 0x1F) << 19)
+
+struct UOItem
+{
+    int RealWidth;
+    int RealHeight;
+
+    int Left;
+    int Top;
+    int Bottom;
+    int Right;
+
+    int GetWidth() const { return Right - Left; }
+    int GetHeight() const { return Bottom - Top; }
+
+    unsigned short **Data; // [ReadlWidth][RealHeight] 32bit color data
+
+    int ItemID;
+    UOItem *pNext;
+};
+
+struct ArtIdx
+{
+    long FilePos;
+    long Length;
+    long Unused;
+};
+
+struct ArtHeader
+{
+    long Unknown;
+    short Width;
+    short Height;
+    //followed by short LookupTable
+};
 
 UOItem *ArtCache = NULL;
 unsigned short **Hues = NULL;
