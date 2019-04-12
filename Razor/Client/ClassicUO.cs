1c6875d69d21916de2bdc0ab9f7d241f2cd40d83
DarkLotus <kklolk@gmail.com>
2019-04-13 07:44:22 +1000

    Start wiring up CUO Client implementation.

--- Razor/Client/ClassicUO.cs
+++ Razor/Client/ClassicUO.cs
@@ -8,17 +8,40 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+using CUO_API;
 
 namespace Assistant
 {
 
     public class ClassicUOClient : Client
     {
-        public override DateTime ConnectionStart { get; }
+        public override DateTime ConnectionStart => m_ConnectionStart;
         public override IPAddress LastConnection { get; }
-        public override Process ClientProcess { get; }
+        public override Process ClientProcess => m_ClientProcess;
+        public override bool ClientRunning => m_ClientRunning;
 
-        public override bool ClientRunning { get; }
+
+        private DateTime m_ConnectionStart;
+        private Process m_ClientProcess = null;
+        private bool m_ClientRunning = false;
+        private string m_ClientVersion;
+        
+        private static OnPacketSendRecv _sendToClient, _sendToServer, _recv, _send;
+        private static OnGetPacketLength _getPacketLength;
+        private static OnGetPlayerPosition _getPlayerPosition;
+        private static OnCastSpell _castSpell;
+        private static OnGetStaticImage _getStaticImage;
+
+        private static OnHotkey _onHotkeyPressed;
+        private static OnMouse _onMouse;
+        private static OnUpdatePlayerPosition _onUpdatePlayerPosition;
+        private static OnClientClose _onClientClose;
+        private static OnInitialize _onInitialize;
+        private static OnConnected _onConnected;
+        private static OnDisconnected _onDisconnected;
+        private static OnFocusGained _onFocusGained;
+        private static OnFocusLost _onFocusLost;
+        private IntPtr m_ClientWindow;
 
         public override void SetMapWndHandle(Form mapWnd)
         {
@@ -54,11 +77,143 @@ namespace Assistant
 
         public override bool ServerEncrypted { get; set; }
 
-        public override bool InstallHooks(IntPtr mainWindow)
+        public unsafe override bool InstallHooks(IntPtr pluginPtr)
         {
-            return false;
+            var header = (PluginHeader*) pluginPtr;
+             _sendToClient = (OnPacketSendRecv)Marshal.GetDelegateForFunctionPointer(header->Recv,typeof(OnPacketSendRecv));
+            _sendToServer = (OnPacketSendRecv)Marshal.GetDelegateForFunctionPointer(header->Send,typeof(OnPacketSendRecv));
+            _getPacketLength = (OnGetPacketLength)Marshal.GetDelegateForFunctionPointer(header->GetPacketLength,typeof(OnGetPacketLength));
+            _getPlayerPosition = (OnGetPlayerPosition)Marshal.GetDelegateForFunctionPointer(header->GetPlayerPosition,typeof(OnGetPlayerPosition));
+            _castSpell = (OnCastSpell)Marshal.GetDelegateForFunctionPointer(header->CastSpell,typeof(OnCastSpell));
+            _getStaticImage = (OnGetStaticImage)Marshal.GetDelegateForFunctionPointer(header->GetStaticImage,typeof(OnGetStaticImage));
+
+            m_ClientVersion = new Version(header->ClientVersion << 24,header->ClientVersion << 16, header->ClientVersion << 8,(byte)header->ClientVersion).ToString(); 
+            m_ClientWindow = header->HWND;
+
+            _recv = OnRecv;
+            _send = OnSend;
+            _onHotkeyPressed = OnHotKeyHandler;
+            _onMouse = OnMouseHandler;
+            _onUpdatePlayerPosition = OnPlayerPositionChanged;
+            _onClientClose = OnClientClosing;
+            _onInitialize = OnInitialize;
+            _onConnected = OnConnected;
+            _onDisconnected = OnDisconnected;
+            //_onFocusGained = OnFocusGained;
+            //_onFocusLost = OnFocusLost;
+
+            header->OnRecv = Marshal.GetFunctionPointerForDelegate(_recv);
+            header->OnSend = Marshal.GetFunctionPointerForDelegate(_send);
+            header->OnHotkeyPressed = Marshal.GetFunctionPointerForDelegate(_onHotkeyPressed);
+            header->OnMouse = Marshal.GetFunctionPointerForDelegate(_onMouse);
+            header->OnPlayerPositionChanged = Marshal.GetFunctionPointerForDelegate(_onUpdatePlayerPosition);
+            header->OnClientClosing = Marshal.GetFunctionPointerForDelegate(_onClientClose);
+            header->OnInitialize = Marshal.GetFunctionPointerForDelegate(_onInitialize);
+            header->OnConnected = Marshal.GetFunctionPointerForDelegate(_onConnected);
+            header->OnDisconnected = Marshal.GetFunctionPointerForDelegate(_onDisconnected);
+            header->OnFocusGained = Marshal.GetFunctionPointerForDelegate(_onFocusGained);
+            header->OnFocusLost = Marshal.GetFunctionPointerForDelegate(_onFocusLost);
+
+			return true;
+        }
+        
+        private void OnPlayerPositionChanged(int x, int y, int z)
+        {
+            World.Player.Position = new Point3D(x, y, z);
         }
 
+        private unsafe bool OnRecv(byte[] data, int length)
+        {
+            fixed (byte* ptr = data)
+            {
+                PacketReader p = new PacketReader(ptr, length, PacketsTable.GetPacketLength(ptr,length) < 0);
+                Packet packet = new Packet(data, length, p.DynamicLength);
+
+                return !PacketHandler.OnServerPacket(p.PacketID, p, packet);
+            }
+        }
+
+        private unsafe bool OnSend(byte[] data, int length)
+        {
+            fixed (byte* ptr = data)
+            {
+                PacketReader p = new PacketReader(ptr, length, PacketsTable.GetPacketLength(ptr,length) < 0);
+                Packet packet = new Packet(data, length, p.DynamicLength);
+
+                return !PacketHandler.OnClientPacket(p.PacketID, p, packet);
+            }
+        }
+        private void OnMouseHandler(int button, int wheel)
+        {
+            if (button > 4)
+                button = 3;
+            else if (button > 3)
+                button = 2;
+            else if (button > 2)
+                button = 2;
+            else if (button > 1)
+                button = 1;
+
+            HotKey.OnMouse(button, wheel);
+        }
+        private bool OnHotKeyHandler(int key, int mod, bool ispressed)
+        {
+            if (ispressed)
+            {
+                return HotKey.OnKeyDown((int)(key | mod));;
+            }
+            return true;
+        }
+        private void OnDisconnected()
+        {
+            PacketHandlers.Party.Clear();
+            //TODO reset window title
+            Engine.MainWindow.UpdateTitle();
+            UOAssist.PostLogout();
+
+            World.Player = null;
+            World.Items.Clear();
+            World.Mobiles.Clear();
+            Macros.MacroManager.Stop();
+            ActionQueue.Stop();
+            Counter.Reset();
+            GoldPerHourTimer.Stop();
+            BandageTimer.Stop();
+            GateTimer.Stop();
+            BuffsTimer.Stop();
+            StealthSteps.Unhide();
+            Engine.MainWindow.OnLogout();
+            if (Engine.MainWindow.MapWindow != null)
+                Engine.MainWindow.MapWindow.Close();
+            PacketHandlers.Party.Clear();
+            PacketHandlers.IgnoreGumps.Clear();
+            Config.Save();
+        }
+        private void OnConnected()
+        {
+            m_ConnectionStart = DateTime.Now;
+           
+        }
+        private void OnClientClosing()
+        {
+            var last = Console.BackgroundColor;
+            var lastFore = Console.ForegroundColor;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Closing Razor instance");
+            Console.BackgroundColor = last;
+            Console.ForegroundColor = lastFore;
+        }
+        private void OnInitialize()
+        {
+            var last = Console.BackgroundColor;
+            var lastFore = Console.ForegroundColor;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Initialized Razor instance");
+            Console.BackgroundColor = last;
+            Console.ForegroundColor = lastFore;
+        }
         public override void SetConnectionInfo(IPAddress addr, int port)
         {
 
@@ -96,7 +251,7 @@ namespace Assistant
 
         public override void SendToServer(Packet p)
         {
-
+            _sendToServer(p.Compile(), (int)p.Length);
         }
 
         public override void SendToServer(PacketReader pr)
@@ -106,12 +261,13 @@ namespace Assistant
 
         public override void SendToClient(Packet p)
         {
+            _sendToClient(p.Compile(), (int)p.Length);
 
         }
 
         public override void ForceSendToClient(Packet p)
         {
-
+            _sendToClient(p.Compile(), (int)p.Length);
         }
 
         public override void SetPosition(uint x, uint y, uint z, byte dir)
@@ -126,12 +282,12 @@ namespace Assistant
 
         public override string GetClientVersion()
         {
-            return "";
+            return m_ClientVersion;
         }
 
         public override IntPtr GetWindowHandle()
         {
-            return IntPtr.Zero;
+            return m_ClientWindow;
         }
 
         public override uint TotalDataIn()
