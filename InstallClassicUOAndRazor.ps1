d12de0175218506af71a478fe223cac0160432e8
Quick <577652+markdwags@users.noreply.github.com>
2019-05-01 15:49:03 -0500

    Fixed issue with install script location

--- InstallClassicUOAndRazor.ps1
+++ InstallClassicUOAndRazor.ps1
@@ -15,7 +15,6 @@ Write-Host ""
 
 # Set the directory to be the one the command was issued in
 $WorkingDir = $(Get-Location).Path
-cd $WorkingDir
 
 Write-Host "This script will install the latest developer versions of ClassicUO and Razor. It can be ran to install for the first time, or to ensure your current versions of ClassicUO and Razor are on the latest developer preview builds.
 
@@ -28,7 +27,6 @@ NOTE: This script will perform the following steps, so please review first:
 WARNING: This script will overwrite ALL files related to ClassicUO and Razor. Backup this folder if you want to preserve this install.
 
 "
-
 $proceed = Read-Host -Prompt "Would you like to continue with the process above? [y or n]"
 
 if ($proceed -ne "Y" -Or $proceed -ne "y") { Exit }
@@ -38,33 +36,33 @@ if ($proceed -ne "Y" -Or $proceed -ne "y") { Exit }
 
 # First, let's get the latest ClassicUO build from GitHub
 Write-Host "Downloading the latest dev build of ClassicUO.."
-Invoke-WebRequest -Uri "https://github.com/andreakarasho/ClassicUO/releases/download/ClassicUO-dev-preview/ClassicUO-dev-preview-release.zip" -OutFile ".\ClassicUO-dev-preview-release.zip" -Verbose
+Invoke-WebRequest -Uri "https://github.com/andreakarasho/ClassicUO/releases/download/ClassicUO-dev-preview/ClassicUO-dev-preview-release.zip" -OutFile "$WorkingDir\ClassicUO-dev-preview-release.zip" -Verbose
 Write-Host "..done"
 
 # Now download the latest Razor build from GitHub
 Write-Host "Downloading the latest dev build of Razor.."
-Invoke-WebRequest -Uri "https://github.com/markdwags/Razor/releases/download/Razor-dev-preview/Razor-dev-preview.zip" -OutFile ".\Razor-dev-preview.zip" -Verbose
+Invoke-WebRequest -Uri "https://github.com/markdwags/Razor/releases/download/Razor-dev-preview/Razor-dev-preview.zip" -OutFile "$WorkingDir\Razor-dev-preview.zip" -Verbose
 Write-Host "..done"
 
 # Extract both ClassicUO and Razor into the folder this script is running in
 Write-Host "Extracting ClassicUO and Razor"
-Expand-Archive -Path ".\ClassicUO-dev-preview-release.zip" -DestinationPath ".\" -Force
-Expand-Archive -Path ".\Razor-dev-preview.zip" -DestinationPath ".\Razor\" -Force
+Expand-Archive -Path "$WorkingDir\ClassicUO-dev-preview-release.zip" -DestinationPath "$WorkingDir\" -Force
+Expand-Archive -Path "$WorkingDir\Razor-dev-preview.zip" -DestinationPath "$WorkingDir\Razor\" -Force
 Write-Host "..done"
 
 # For some users, the files are "Blocked" and require to be unblock to run Razor with CUO correctly. Unblock both folders just in case.
 Write-Host "Unblocking ClassicUO and Razor files.."
-Get-ChildItem -Path ".\" | Unblock-File
-Get-ChildItem -Path ".\Razor\" | Unblock-File
+Get-ChildItem -Path "$WorkingDir\" | Unblock-File
+Get-ChildItem -Path "$WorkingDir\Razor\" | Unblock-File
 Write-Host "..done"
 
 # If the settings.json file for ClassicUO doesn't exist, run ClassicUO so it auto-generates
-if ($(Test-Path ".\settings.json" -PathType Leaf) -eq $False) {
+if ($(Test-Path "$WorkingDir\settings.json" -PathType Leaf) -eq $False) {
 
   # Launch ClassicUO to generate default file
   Write-Host "Couldn't find settings.json, launching ClassicUO to create a new one."
   Write-Host "*** PLEASE CLICK THE OK WHEN PROMPTED BY CLASSICUO ***"
-  Start-Process -FilePath ".\ClassicUO.exe" -Wait
+  Start-Process -FilePath "$WorkingDir\ClassicUO.exe" -Wait
 
   Write-Host "Setting up settings.json"  
   Write-Host "*** HIT ENTER TO ACCEPT DEFAULT VALUES ***"
@@ -91,7 +89,7 @@ if ($(Test-Path ".\settings.json" -PathType Leaf) -eq $False) {
   }
 
   # Update the default settings.json file to point to where we installed Razor
-  $cuoSettings = Get-Content ".\settings.json" -Raw | ConvertFrom-Json
+  $cuoSettings = Get-Content "$WorkingDir\settings.json" -Raw | ConvertFrom-Json
 
   # Set the values based on the input from above
   $cuoSettings."ip" = $serverhost
@@ -104,7 +102,7 @@ if ($(Test-Path ".\settings.json" -PathType Leaf) -eq $False) {
 
   # Save the settings back to the file
   Write-Host "Saving input to settings.json"  
-  $cuoSettings | ConvertTo-Json -Depth 32 | Set-Content ".\settings.json"
+  $cuoSettings | ConvertTo-Json -Depth 32 | Set-Content "$WorkingDir\settings.json"
 
 } else {
 
@@ -121,7 +119,7 @@ if ($(Test-Path "$Home\Desktop\ClassicUO.lnk" -PathType Leaf) -eq $False) {
   if ($createShortcut -eq "Y" -Or $createShortcut -eq "y") {
     $WshShell = New-Object -comObject WScript.Shell
     $shortcut = $WshShell.CreateShortcut("$Home\Desktop\ClassicUO.lnk")
-    $shortcut.TargetPath = "$PSScriptRoot\ClassicUO.exe"
+    $shortcut.TargetPath = "$WorkingDir\ClassicUO.exe"
     $shortcut.Save()
   }  
 }
@@ -131,7 +129,7 @@ $launch = Read-Host -Prompt "Would you like to launch ClassicUO with Razor? [y o
 
 if ($launch -eq "Y" -Or $launch -eq "y") {
   Write-Host "Launching ClassicUO.exe"
-  Start-Process -FilePath ".\ClassicUO.exe"
+  Start-Process -FilePath "$WorkingDir\ClassicUO.exe"
 }
 
 Write-Host "Press ENTER to exit or close this window"
