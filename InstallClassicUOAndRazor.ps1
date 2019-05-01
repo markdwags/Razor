572a736a93c9762c3aa03b6a1dbd2c07e80deb2e
Quick <577652+markdwags@users.noreply.github.com>
2019-05-01 14:55:25 -0500

    Added appveyor and powershell script to install CUO and Razor

---
+++ InstallClassicUOAndRazor.ps1
@@ -0,0 +1,134 @@
+<#
+.SYNOPSIS
+  A basic Powershell script to download ClassicUO and Razor
+  and configure the basic settings.
+.NOTES
+  Version:        1.0
+  Author:         Quick
+  Creation Date:  5/1/2019
+#>
+
+Write-Host "InstallClassicUOAndRazor.ps1"
+Write-Host "v1.0"
+Write-Host "Author: Quick (https://github.com/markdwags/Razor)"
+Write-Host ""
+
+Write-Host "This script will install the latest developer versions of ClassicUO and Razor. It can be ran to install for the first time, or to ensure your current versions of ClassicUO and Razor are on the latest developer preview builds.
+
+NOTE: This script will perform the following steps, so please review first:
+
+1) Download the latest ClassicUO dev preview build to: $PSScriptRoot
+2) Download the latest Razor dev preview to: $PSScriptRoot\Razor\
+3) Help with setting up the initial settings.json file if it doesn't exist
+
+WARNING: This script will overwrite ALL files related to ClassicUO and Razor. Backup this folder if you want to preserve this install.
+
+"
+
+$proceed = Read-Host -Prompt "Would you like to continue with the process above? [y or n]"
+
+if ($proceed -ne "Y" -Or $proceed -ne "y") { Exit }
+
+# Set TLS to be v1.2 or Invoke-WebRequest will fail
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# First, let's get the latest ClassicUO build from GitHub
+Write-Host "Downloading the latest dev build of ClassicUO.."
+Invoke-WebRequest -Uri "https://github.com/andreakarasho/ClassicUO/releases/download/ClassicUO-dev-preview/ClassicUO-dev-preview-release.zip" -OutFile ".\ClassicUO-dev-preview-release.zip" -Verbose
+Write-Host "..done"
+
+# Now download the latest Razor build from GitHub
+Write-Host "Downloading the latest dev build of Razor.."
+Invoke-WebRequest -Uri "https://github.com/markdwags/Razor/releases/download/Razor-dev-preview/Razor-dev-preview.zip" -OutFile ".\Razor-dev-preview.zip" -Verbose
+Write-Host "..done"
+
+# Extract both ClassicUO and Razor into the folder this script is running in
+Write-Host "Extracting ClassicUO and Razor"
+Expand-Archive -Path ".\ClassicUO-dev-preview-release.zip" -DestinationPath ".\" -Force
+Expand-Archive -Path ".\Razor-dev-preview.zip" -DestinationPath ".\Razor\" -Force
+Write-Host "..done"
+
+# For some users, the files are "Blocked" and require to be unblock to run Razor with CUO correctly. Unblock both folders just in case.
+Write-Host "Unblocking ClassicUO and Razor files.."
+Get-ChildItem -Path ".\" | Unblock-File
+Get-ChildItem -Path ".\Razor\" | Unblock-File
+Write-Host "..done"
+
+# If the settings.json file for ClassicUO doesn't exist, run ClassicUO so it auto-generates
+if ($(Test-Path ".\settings.json" -PathType Leaf) -eq $False) {
+
+  # Launch ClassicUO to generate default file
+  Write-Host "Couldn't find settings.json, launching ClassicUO to create a new one."
+  Write-Host "*** PLEASE CLICK THE OK WHEN PROMPTED BY CLASSICUO ***"
+  Start-Process -FilePath ".\ClassicUO.exe" -Wait
+
+  Write-Host "Setting up settings.json"  
+  Write-Host "*** HIT ENTER TO ACCEPT DEFAULT VALUES ***"
+
+  # User input for the various data points for settings.json
+  $serverhost = Read-Host -Prompt "> Enter the host/ip of the UO server (default: login.uorenaissance.com)"
+  if ([string]::IsNullOrEmpty($serverhost)) {
+    $serverhost = "login.uorenaissance.com"
+  }
+
+  $port = Read-Host -Prompt "> Enter the port of the UO server (default: 2593)"
+  if ([string]::IsNullOrEmpty($port)) {
+    $port = 2593
+  }
+
+  $path = Read-Host -Prompt "> Enter the path to your Ultima Online files (default: C:\Ultima Online)" 
+  if ([string]::IsNullOrEmpty($path)) {
+    $path = "C:\Ultima Online"
+  }
+
+  $client = Read-Host -Prompt "> Enter the client version you are using (default: 5.0.8.3)"
+  if ([string]::IsNullOrEmpty($client)) {
+    $client = "5.0.8.3"
+  }
+
+  # Update the default settings.json file to point to where we installed Razor
+  $cuoSettings = Get-Content ".\settings.json" -Raw | ConvertFrom-Json
+
+  # Set the values based on the input from above
+  $cuoSettings."ip" = $serverhost
+  $cuoSettings."port" = [int]$port
+  $cuoSettings."ultimaonlinedirectory" = $path
+  $cuoSettings."clientversion" = $client
+
+  # Set plugin location to point to Razor
+  $cuoSettings."plugins"[0] = "$PSScriptRoot\Razor\Razor.exe"
+
+  # Save the settings back to the file
+  Write-Host "Saving input to settings.json"  
+  $cuoSettings | ConvertTo-Json -Depth 32 | Set-Content ".\settings.json"
+
+} else {
+
+  Write-Host "settings.json exists, no changes made"
+
+}
+
+Write-Host "The latest developer version of ClassicUO and Razor are installed."
+
+# Let's create a shortcut on the Desktop if one doesn't exist
+if ($(Test-Path "$Home\Desktop\ClassicUO.lnk" -PathType Leaf) -eq $False) {
+  $createShortcut = Read-Host -Prompt "Would you like create a shortcut on your Desktop? [y or n]"
+
+  if ($createShortcut -eq "Y" -Or $createShortcut -eq "y") {
+    $WshShell = New-Object -comObject WScript.Shell
+    $shortcut = $WshShell.CreateShortcut("$Home\Desktop\ClassicUO.lnk")
+    $shortcut.TargetPath = "$PSScriptRoot\ClassicUO.exe"
+    $shortcut.Save()
+  }  
+}
+
+# Should we launch ClassicUO now?
+$launch = Read-Host -Prompt "Would you like to launch ClassicUO with Razor? [y or n]"
+
+if ($launch -eq "Y" -Or $launch -eq "y") {
+  Write-Host "Launching ClassicUO.exe"
+  Start-Process -FilePath ".\ClassicUO.exe"
+}
+
+Write-Host "Press ENTER to exit or close this window"
+Read-Host
