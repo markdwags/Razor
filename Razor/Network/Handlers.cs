75e8a207ad77a3ee900e4fdf0b901adbb8b002af
Quick <577652+markdwags@users.noreply.github.com>
2019-11-19 12:44:30 -0600

    Remove packet block on 0xF0 when using ClassicUO

--- Razor/Network/Handlers.cs
+++ Razor/Network/Handlers.cs
@@ -2323,7 +2323,9 @@ namespace Assistant
 
         private static void RunUOProtocolExtention(PacketReader p, PacketHandlerEventArgs args)
         {
-            args.Block = true;
+            // Only block this packet if they are using the OSI client
+            // since ClassicUO actually has support for it now
+            args.Block = Client.IsOSI;
 
             switch (p.ReadByte())
             {
