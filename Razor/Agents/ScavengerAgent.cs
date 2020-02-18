348d68c89637d9621edfe67e633c8b8b0bf6ff45
Quick <577652+markdwags@users.noreply.github.com>
2020-02-18 11:04:23 -0600

    Moved class to correct folder [chore]

--- Razor/Agents/ScavengerAgent.cs
+++ Razor/Agents/ScavengerAgent.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿#region license
+
+// Razor: An Ultima Online Assistant
+// Copyright (C) 2020 Razor Development Community on GitHub <https://github.com/markdwags/Razor>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Xml;
 using Assistant.Agents;
 using Assistant.UI;
 
-namespace Assistant
+namespace Assistant.Agents
 {
     public class ScavengerAgent : Agent
     {
@@ -57,6 +77,7 @@ namespace Assistant
             UpdateEnableButton();
             World.Player.SendMessage(MsgLevel.Force, "Scavenger Agent Disabled");
         }
+
         public void Enable()
         {
             m_Enabled = true;
@@ -359,7 +380,7 @@ namespace Assistant
             for (int i = 0; i < m_Items.Count; i++)
             {
                 xml.WriteStartElement("item");
-                xml.WriteAttributeString("id", ((ItemID)m_Items[i]).Value.ToString());
+                xml.WriteAttributeString("id", ((ItemID) m_Items[i]).Value.ToString());
                 xml.WriteEndElement();
             }
         }
@@ -389,7 +410,7 @@ namespace Assistant
                 try
                 {
                     string iid = el.GetAttribute("id");
-                    m_Items.Add((ItemID)Convert.ToUInt16(iid));
+                    m_Items.Add((ItemID) Convert.ToUInt16(iid));
                 }
                 catch
                 {
--- Razor/Core/ActionQueue.cs
+++ Razor/Core/ActionQueue.cs
@@ -22,6 +22,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Assistant.Agents;
 
 namespace Assistant
 {
