3c3ae42f3c9daea6cc4b1a26eedf989cd8bc7197
Quick <577652+markdwags@users.noreply.github.com>
2021-10-14 14:15:00 -0500

    [chore] Inject build number

---
+++ AssemblyVersion.ps1
@@ -0,0 +1,16 @@
+﻿$file = "./Razor/Properties/AssemblyInfo.cs"
+$version = "1.7.2.$Env:GITHUB_RUN_ID"
+
+$fileObject = Get-Item $file
+
+Write-Verbose "Changing version in $($fileObject.FullName) to $version"
+
+$sr = New-Object System.IO.StreamReader($fileObject, [System.Text.Encoding]::GetEncoding("utf-8"))
+$content = $sr.ReadToEnd()
+$sr.Close()
+
+$content = [Regex]::Replace($content, "(\d+)\.(\d+)\.(\d+)[\.(\d+)]*", $version);
+
+$sw = New-Object System.IO.StreamWriter($fileObject, $false, [System.Text.Encoding]::GetEncoding("utf-8"))
+$sw.Write($content)
+$sw.Close()
