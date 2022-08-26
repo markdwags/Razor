41a45f761c4846946d376f5a530c7fd93bda6e44
Quick <577652+markdwags@users.noreply.github.com>
2022-08-25 19:54:45 -0500

    Restore correct license header

--- Razor/Gumps/GumpPage.cs
+++ Razor/Gumps/GumpPage.cs
@@ -1,19 +1,20 @@
 ﻿#region license
-// Razor: An Ultima Online Assistant
-// Copyright (c) 2022 Razor Development Community on GitHub <https://github.com/markdwags/Razor>
-// 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/* Copyright (C) 2009 Matthew Geyer
+ * 
+ * This file is part of UO Machine.
+ * 
+ * UO Machine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * UO Machine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with UO Machine.  If not, see <http://www.gnu.org/licenses/>. */
 #endregion
 
 using System.Linq;
