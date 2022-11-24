930087e481638f1f6e13ac9c112689524158f0e6
Quick <577652+markdwags@users.noreply.github.com>
2022-11-24 09:04:53 -0600

    Fixed issue with findtype when using or and as

--- Razor/Scripts/Engine/Interpreter.cs
+++ Razor/Scripts/Engine/Interpreter.cs
@@ -948,7 +948,12 @@ namespace Assistant.Scripts.Engine
                     case ASTNodeType.IN:
                         return ((string)rhs).Contains((string)lhs);
                     case ASTNodeType.AS:
-                        Interpreter.SetVariable(rhs.ToString(), lhs.ToString());
+
+                        if ((uint) lhs > 0)
+                        {
+                            Interpreter.SetVariable(rhs.ToString(), lhs.ToString());
+                        }
+
                         return CompareOperands(ASTNodeType.EQUAL, lhs, true);
                 }
             }
